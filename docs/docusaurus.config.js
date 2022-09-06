d7a8e89f105c35d68c82eb55f0734a75963d7ad6
romanbotnari <romanbotnari@me.com>
2022-09-06 10:03:33 +0300

    update path

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'raumd',
   tagline: 'Your python based shell spaceship',
   url: 'https://romanbotnari.github.io/',
-  baseUrl: '/',
+  baseUrl: '/raumd/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
