b2be57d42b33c0167735f7078840764e218036ef
romanbotnari <romanbotnari@me.com>
2022-09-06 11:51:15 +0300

    update build

--- docs/assets/js/1fbda1a2.5b3afe48.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkraumdeuter_info=self.webpackChunkraumdeuter_info||[]).push([[35],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- docs/assets/js/7f637689.8f976d93.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkraumdeuter_info=self.webpackChunkraumdeuter_info||[]).push([[865],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
